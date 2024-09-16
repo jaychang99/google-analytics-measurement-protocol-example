bc05b25e6a32da8a6e4c34213b4333ae5cbe157b
jaychang99 <jaychang99@gmail.com>
2024-09-16 16:10:03 +0900

    feat(frontend): add google analytics client-side

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -25,6 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* <!-- Google tag (gtag.js) --> */}
+      <GoogleAnalytics gaId="G-R2PTPWL3V1" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
